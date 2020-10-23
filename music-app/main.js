1d47c28bdeb4a982a9ff0c07cb762c4705dd2ab4
Mohamed Ahmed <36579933+leyam2468@users.noreply.github.com>
2020-10-23 10:40:14 +0200

    add some in music-app

--- music-app/main.js
+++ music-app/main.js
@@ -1,13 +1,66 @@
 const playlist = document.querySelector('.playlist');
+
+const mySong = [
+	{
+		name: 'مهرجان هنعمل لغبطيطا',
+		time: '4:47',
+		srcImg:
+			'https://i1.sndcdn.com/artworks-Dwz7wx03EaJYynlt-M3ZH2Q-t500x500.jpg',
+		src:
+			'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/903123313&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+	},
+	{
+		name: 'Ed Sheeran - Perfect',
+		time: '5:47',
+		srcImg:
+			'https://i1.sndcdn.com/artworks-000210714414-u31uaj-t500x500.jpg',
+		src:
+			'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/310640631&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+	},
+];
+
+let Addsongs = (song) => {
+	// Create Content Elm Firest
+	let audioEl = `
+	<img
+	src="${song.srcImg}"
+	alt="${song.name}"
+	width="60px"
+	/>
+	<div class="music-name">
+		<span class="name">${song.name}</span>
+		<span class="time">${song.time}</span>
+	</div>
+	<div class="music-options">
+		<i class="fas fa-ellipsis-v"></i>
+	</div>`;
+	// Create Perant El For Content
+	let audioItem = document.createElement('div');
+	// Add Class
+	audioItem.classList.add('music-item');
+	// Add data Atttr
+	audioItem.setAttribute('data-src', song.src);
+	// add Cont To Parent
+	audioItem.innerHTML = audioEl;
+	// Add All To Page
+	playlist.appendChild(audioItem);
+};
+for (const song of mySong) {
+	Addsongs(song);
+}
+
 const musicItem = document.querySelectorAll('.music-item');
-const audio = document.querySelectorAll('.audio');
+const audioPlayer = document.querySelector('.audio-player');
 
 musicItem.forEach((item, index) => {
 	item.addEventListener('click', function () {
+		// Remover Active Class
+		musicItem.forEach((item) => {
+			item.classList.remove('active');
+		});
+		// Add Active Class
 		this.className += ' active';
-		// item.classList.remove('active');
-		// item[index].classList.add('active');
-		// audio[index].currentTime = 0;
-		// audio[index].play();
+		// Add audio src To Audio Player
+		audioPlayer.src = this.getAttribute('data-src');
 	});
 });
