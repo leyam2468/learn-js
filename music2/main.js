3ebfb0782d315c7f4853b74a1b3aa4fc11afff84
unknown <leyamm3@gmail.com>
2020-11-09 20:39:25 +0200

    add music

--- music2/main.js
+++ music2/main.js
@@ -1,44 +1,74 @@
-const app = () => {
-	const song = document.querySelector('.song');
-	const time = document.querySelector('.time');
-	const play = document.querySelector('.play');
+const playlist = document.querySelector(".playlist");
 
-	const sounds = document.querySelectorAll('.sound-picker button');
+const mySong = [
+	{
+		name: "مهرجان هنعمل لغبطيطا",
+		time: "4:47",
+		srcImg:
+			"https://i1.sndcdn.com/artworks-Dwz7wx03EaJYynlt-M3ZH2Q-t500x500.jpg",
+		src:
+			"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/915556366&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+	},
+	{
+		name: "انتى بسكوتايه مقرمشة",
+		time: "4:06",
+		srcImg:
+			"https://i1.sndcdn.com/artworks-Jvxun4gfVEphV9X9-wt8XHQ-t500x500.jpg",
+		src:
+			"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/915558670&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+	},
+	{
+		name: "محمد رمضان - يا حبيبي",
+		time: "4:44",
+		srcImg:
+			"https://i1.sndcdn.com/artworks-bqIxvOF1GhGkeaFI-n9y7HA-t500x500.jpg",
+		src:
+			"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/903401953&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+	},
+];
 
-	sounds.forEach((sound) => {
-		sound.addEventListener('click', function () {
-			song.src = this.getAttribute('data-src');
-
-			sounds.forEach((soun) => {
-				soun.classList.remove('active');
-			});
-			this.classList.add('active');
-		});
-	});
-	// Controler
-	play.addEventListener('click', () => {
-		if (song.paused) {
-			song.play();
-			play.classList.replace('fa-play', 'fa-pause');
-		} else {
-			song.pause();
-			play.classList.replace('fa-pause', 'fa-play');
-		}
-
-		song.ontimeupdate = () => {
-			let currentTime = song.currentTime;
-
-			time.innerText = `${Math.floor(currentTime / 60)}:${Math.floor(
-				currentTime % 60
-			)} // ${Math.floor(song.duration / 60)}:${Math.floor(
-				song.duration % 60
-			)}`;
-
-			if (currentTime >= song.duration) {
-				song.pause();
-				play.classList.replace('fa-pause', 'fa-play');
-			}
-		};
-	});
+let Addsongs = (song) => {
+	// Create Content Elm Firest
+	let audioEl = `
+	<img
+	src="${song.srcImg}"
+	alt="${song.name}"
+	width="60px"
+	/>
+	<div class="music-name">
+		<span class="name">${song.name}</span>
+		<span class="time">${song.time}</span>
+	</div>
+	<div class="music-options">
+		<i class="fas fa-ellipsis-v"></i>
+	</div>`;
+	// Create Perant El For Content
+	let audioItem = document.createElement("div");
+	// Add Class
+	audioItem.classList.add("music-item");
+	// Add data Atttr
+	audioItem.setAttribute("data-src", song.src);
+	// add Cont To Parent
+	audioItem.innerHTML = audioEl;
+	// Add All To Page
+	playlist.appendChild(audioItem);
 };
-app();
+for (const song of mySong) {
+	Addsongs(song);
+}
+
+const musicItem = document.querySelectorAll(".music-item");
+const audioPlayer = document.querySelector(".audio-player");
+
+musicItem.forEach((item, index) => {
+	item.addEventListener("click", function () {
+		// Remover Active Class
+		musicItem.forEach((item) => {
+			item.classList.remove("active");
+		});
+		// Add Active Class
+		this.className += " active";
+		// Add audio src To Audio Player
+		audioPlayer.src = this.getAttribute("data-src");
+	});
+});
