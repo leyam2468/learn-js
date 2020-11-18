ba1a42959c82672e7037bd942536535d0c649d6d
unknown <leyamm3@gmail.com>
2020-11-18 19:51:25 +0200

    add text to countdown app

--- countdown/main.js
+++ countdown/main.js
@@ -1,36 +1,30 @@
+let targetDay = new Date("13 jun 2021").getTime();
+const counter = setInterval(() => {
+	let nowTime = new Date().getTime();
 
-			// countDown*****************************
+	let distance = targetDay - nowTime;
+	// Days
+	let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	document.getElementById("days").innerHTML = days;
+	// Hours
+	let hours = Math.floor(
+		(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+	);
+	document.getElementById("hours").innerHTML = hours;
+	// minutes
+	let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	document.getElementById("mins").innerHTML = minutes;
+	// sac
+	let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	document.getElementById("sec").innerHTML = seconds;
 
-			let targetDay = new Date("13 jun 2021").getTime();
-			const counter = setInterval(() => {
-				let nowTime = new Date().getTime();
+	//Days distance / (1000 * 60 * 60 * 24)
+	// hours (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+	// min (distance % (1000 * 60 * 60)) / (1000 * 60)
+	// sac (distance % (1000 * 60)) / 1000
 
-				let distance = targetDay - nowTime;
-				// Days
-				let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-				document.getElementById("days").innerHTML = days;
-				// Hours
-				let hours = Math.floor(
-					(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-				);
-				document.getElementById("hours").innerHTML = hours;
-				// minutes
-				let minutes = Math.floor(
-					(distance % (1000 * 60 * 60)) / (1000 * 60)
-				);
-				document.getElementById("mins").innerHTML = minutes;
-				// sac
-				let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-				document.getElementById("sec").innerHTML = seconds;
-
-				//Days distance / (1000 * 60 * 60 * 24)
-				// hours (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-				// min (distance % (1000 * 60 * 60)) / (1000 * 60)
-				// sac (distance % (1000 * 60)) / 1000
-
-				if (distance < 0) {
-					clearInterval(counter);
-					document.getElementById("counter").innerHTML =
-						"Happy BirthDay";
-				}
-			}, 1000);
+	if (distance < 0) {
+		clearInterval(counter);
+		document.getElementById("counter").innerHTML = "Happy BirthDay";
+	}
+}, 1000);
